f6df10821d7a0831953777d8775bc2a83f5fa1b6
Omni <98383696+omni-aa@users.noreply.github.com>
2025-02-03 03:31:15 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Navbar} from "./Components/Navbar/navbar.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div className="font-bold text-xl">
+          Hello World!
+      </div>
+        <Navbar/>
+    </>
+  )
+}
+
+export default App
